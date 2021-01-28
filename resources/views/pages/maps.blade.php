bfaa83a82b4460c9e8b979b559c1104a7b65ccc1
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-01-27 22:46:17 -0300

    corração mapa

--- resources/views/pages/maps.blade.php
+++ resources/views/pages/maps.blade.php
@@ -7,34 +7,6 @@
                 <div class="card-header">
                     Google Maps
                 </div>
-                <?php header("Refresh:10;"); ?>
-
-
-                <div class="container" style="color:white">
-                    <?php foreach($trend as $trend) {?>
-
-                    <?php $compra = 0; $venda = 0;?>
-                    <h2 class="text-center"><?= $trend->time; ?></h2>
-
-                    <?php $trend->ma5 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <?php $trend->ma10 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <?php $trend->ma20 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <?php $trend->ma50 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <?php $trend->ma100 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <?php $trend->ma200 == 'Venda' ? $venda = $venda + 16.6 : $compra = $compra + 16.6?>
-                    <div class="container">
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-success" role="progressbar"
-                                 style="width:<?= $compra ?>%; background-color:green">
-                                Compra
-                            </div>
-                            <div class="progress-bar progress-bar-danger" role="progressbar"
-                                 style="width:<?= $venda ?>%; background-color:red">
-                                Venda
-                            </div>
-                        </div>
-                    </div>
-                    <?php }?>
                     <div class="card-body">
                         <div id="map" class="map"></div>
                     </div>
