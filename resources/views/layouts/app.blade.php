fcc60f2dcdaaea94ad5077c807d860649a796f9f
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-01-28 09:58:44 -0300

    nome na barra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,10 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Black Dashboard') }}</title>
+        <title>{{ config('app.name', 'FTX') }}</title>
         <!-- Favicon -->
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('black') }}/img/logoban.jpeg">
-        <link rel="icon" type="image/png" href="{{ asset('black') }}/img/logoban.jpeg">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('black') }}/img/logopreta.jpeg">
+        <link rel="icon" type="image/png" href="{{ asset('black') }}/img/logopreta.jpeg">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
         <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
