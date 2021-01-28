1e8236d8f1e1d5e6029def378075aba7b94235b2
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-01-28 06:50:10 -0300

    home melhorada

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,23 +2,28 @@
     <div class="container-fluid">
         <ul class="nav">
             <li class="nav-item">
-                <a href="https://www.instagram.com/ftx_palmas/?hl=pt-br" target="blank" class="nav-link">
-                    {{ __('Instagram') }}
-                </a>
+                <button onclick="window.open('https://www.instagram.com/ftx_palmas/?hl=pt-br')" class="btn btn-icon btn-round btn-instagram">
+                    <i class="fab fa-instagram"></i>
+                </button>
             </li>
             <li class="nav-item">
+                <button onclick="window.open('https://www.facebook.com/FTXPalmas')" class="btn btn-icon btn-round btn-facebook">
+                    <i class="fab fa-facebook"></i>
+                </button>
+                <!--
                 <a href="https://www.facebook.com/FTXPalmas" target="blank" class="nav-link">
                     {{ __('Facebook') }}
                 </a>
+                -->
+            </li>
+            <li class="nav-item">
+                <button onclick="window.open('http://xadrez-tocantins.blogspot.com/')" class="btn btn-icon btn-round btn-blogger">
+                    <i class="fab fa-blogger"></i>
+                </button>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
                     {{ __('Sobre') }}
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="http://xadrez-tocantins.blogspot.com/" target="blank" class="nav-link">
-                    {{ __('Blog') }}
                 </a>
             </li>
         </ul>
