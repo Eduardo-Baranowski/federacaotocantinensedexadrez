fc36d10610e85611e687b031e9086d68d4135d02
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-02-01 11:16:09 -0300

    calendário2

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -21,13 +21,13 @@
             <div class="col-lg-8 ml-auto mr-auto">
               <div class="row">
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','left')">{{ date('d-m-Y') }}</button>
+                    <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','left')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y') }}</i></button>
                 </div>
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','center')">{{ date('d-m-Y', strtotime('+1 days', strtotime(date('d-m-Y')))) }}</button>
+                    <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','center')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y', strtotime('+1 days', strtotime(date('d-m-Y')))) }}</i></button>
                 </div>
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','right')">{{ date('d-m-Y', strtotime('+2 days', strtotime(date('d-m-Y')))) }}</button>
+                    <button class="btn btn-primary btn-block" onclick="demo.showNotification('top','right')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y', strtotime('+2 days', strtotime(date('d-m-Y')))) }}</i></button>
                 </div>
               </div>
             </div>
@@ -36,13 +36,13 @@
             <div class="col-lg-8 ml-auto mr-auto">
               <div class="row">
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','left')">{{ date('d-m-Y', strtotime('+3 days', strtotime(date('d-m-Y')))) }}</button>
+                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','left')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y', strtotime('+3 days', strtotime(date('d-m-Y')))) }} </i></button>
                 </div>
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','center')">{{ date('d-m-Y', strtotime('+4 days', strtotime(date('d-m-Y')))) }}</button>
+                    <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','center')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y', strtotime('+4 days', strtotime(date('d-m-Y')))) }} </i></button>
                 </div>
                 <div class="col-md-4">
-                  <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','right')">{{ date('d-m-Y', strtotime('+5 days', strtotime(date('d-m-Y')))) }}</button>
+                    <button class="btn btn-primary btn-block" onclick="demo.showNotification('bottom','right')"><i class="tim-icons icon-calendar-60"> {{ date('d-m-Y', strtotime('+5 days', strtotime(date('d-m-Y')))) }} </i></button>
                 </div>
               </div>
             </div>
